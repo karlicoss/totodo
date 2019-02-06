b51d6b59c95e5e3c1da87e87524dae341a733a38
Dima Gerasimov <karlicoss@gmail.com>
2019-02-06 23:07:52 +0000

    generic function to add items

---
+++ add2org.py
@@ -0,0 +1,44 @@
+import logging
+from pathlib import Path
+from typing import TypeVar, Generic, Dict, Sequence, Union, List, Tuple
+
+from kython.klogging import setup_logzero
+from kython.ktyping import PathIsh
+from kython.kos import atomic_append
+from kython.state import JsonState
+from kython.org_tools import append_org_entry
+
+Key = str
+# I = TypeVar('I')
+FormattedOrg = str # TODO perhaps we'll do something smarter later...
+I = FormattedOrg
+LoggerIsh = Union[str, logging.Logger]
+
+def add2org(
+        items: Union[List[Tuple[Key, I]], Dict[Key, I]],
+        output: PathIsh,
+        state: PathIsh,
+        logger: LoggerIsh,
+        org_formatter=None,
+):
+    if isinstance(items, dict):
+        items = list(items.items())
+    assert len(set(i[0] for i in items)) == len(items), 'Duplicate keys!!'
+
+    output = Path(output)
+    json_state = JsonState(Path(state))
+
+    if isinstance(logger, str):
+        logger = logging.getLogger(logger)
+        setup_logzero(logger, level=logging.DEBUG)
+
+    for k, i in items:
+        if k in json_state:
+            logger.debug('already handled: %s %s', k, i)
+        else:
+            logger.info('adding: %s %s', k, i)
+            atomic_append(
+                output,
+                i,
+            )
+            json_state[k] = repr(i)
---
+++ hyp2org.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from kython.org_tools import as_org_entry
+from my.hypothesis import get_todos
+
+from add2org import add2org
+
+
+def main():
+    todos = get_todos()
+
+    # TODO meh. maybe, use abstract OrgNote instead?
+    items = [
+        (
+            t.eid,
+            as_org_entry(
+                heading=t.annotation,
+                tags=['hyp2org', *t.tags],
+                body=t.context,
+                created=t.dt,
+                todo=True,
+            ),
+        ) for t in todos
+    ]
+
+
+    add2org(
+        items=items,
+        output='/tmp/hyp2org.org',
+        state='/tmp/hyp2org.json',
+        logger='hyp2org',
+    )
+
+
+if __name__ == '__main__':
+    main()
